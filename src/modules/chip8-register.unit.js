9d7b45448f8cf621ec136410e56c0c6d07a498af
concreted <arichuang@gmail.com>
2015-08-15 17:46:22 -0700

    Add Chip8Register module

---
+++ src/modules/chip8-register.unit.js
@@ -0,0 +1,37 @@
+var chai = require('chai');
+var sinon = require('sinon');
+var sinonChai = require('sinon-chai');
+
+var expect = chai.expect;
+var assert = chai.assert;
+chai.use(sinonChai);
+
+var Chip8Register = require('./chip8-register');
+
+describe('Chip8Register Module', function() {
+  var register = new Chip8Register();
+
+  describe('attributes', function() {
+    it('should have an I register', function() {
+      expect(register.I).to.equal(0);
+    });
+    it('should have registers V0-VF', function() {
+      expect(register.V0).to.equal(0);
+      expect(register.V1).to.equal(0);
+      expect(register.V2).to.equal(0);
+      expect(register.V3).to.equal(0);
+      expect(register.V4).to.equal(0);
+      expect(register.V5).to.equal(0);
+      expect(register.V6).to.equal(0);
+      expect(register.V7).to.equal(0);
+      expect(register.V8).to.equal(0);
+      expect(register.V9).to.equal(0);
+      expect(register.VA).to.equal(0);
+      expect(register.VB).to.equal(0);
+      expect(register.VC).to.equal(0);
+      expect(register.VD).to.equal(0);
+      expect(register.VE).to.equal(0);
+      expect(register.VF).to.equal(0);
+    });
+  });
+});
