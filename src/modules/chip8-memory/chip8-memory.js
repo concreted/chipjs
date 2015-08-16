0eb7bd22a287a17e0cee1f094df94c837ba0a940
concreted <arichuang@gmail.com>
2015-08-15 18:41:09 -0700

    Refactor Chip8Memory into generic memory module

--- src/modules/chip8-memory/chip8-memory.js
+++
@@ -1,9 +0,0 @@
-function Chip8Memory() {
-  this.RAM = new Uint8Array(4096);
-}
-
-Chip8Memory.prototype = {
-
-}
-
-module.exports = Chip8Memory;
---
+++ src/modules/memory/memory.js
@@ -0,0 +1,5 @@
+module.exports = {
+  create8Bit: function(n) {
+    return new Uint8Array(n);
+  }
+}
