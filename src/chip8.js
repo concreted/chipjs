76027e0ded2c285dd64f7f1dd765fcfda0d2d142
concreted <arichuang@gmail.com>
2016-01-31 16:18:52 -0800

    Add video module using HTML5 Canvas

--- src/chip8.js
+++ src/chip8.js
@@ -2,6 +2,7 @@ var memory = require('./modules/memory');
 var constants = require('./modules/chip8-constants');
 var Chip8Registers = require('./modules/chip8-registers');
 var Chip8Timers = require('./modules/chip8-timers');
+var VideoCanvas = require('./modules/video-canvas');
 
 function Chip8(beep) {
   this.ram = memory.create8Bit(constants.MEMORY_SIZE_BYTES);
@@ -9,16 +10,50 @@ function Chip8(beep) {
   this.timers = new Chip8Timers(beep);
   this.registers = new Chip8Registers();
 
-  this.video = memory.create8Bit2D(constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT);
+  this.videoBuffer = memory.create8Bit2D(
+    constants.SCREEN_WIDTH,
+    constants.SCREEN_HEIGHT
+  );
+
+  this.video = new VideoCanvas(
+    document.getElementById('chip8'),
+    constants.SCREEN_WIDTH,
+    constants.SCREEN_HEIGHT,
+    5,
+    5
+  );
 }
 
 Chip8.prototype = {
   start: function() {
     this.timers.start();
+
+    this._mainLoopInterval = setInterval(
+      this._mainLoop.bind(this),
+      constants.REFRESH_MS
+    );
   },
 
   stop: function() {
     this.timers.stop();
+    clearInterval(this._mainLoopInterval);
+  },
+
+  cycle: function() {
+
+  },
+
+  draw: function() {
+    this.video.drawBuffer(this.videoBuffer);
+  },
+
+  loadROM: function(rom) {
+
+  },
+
+  _mainLoop: function() {
+    this.cycle();
+    this.draw();
   }
 }
 
